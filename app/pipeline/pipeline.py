f79106f57fa76e6481b26866c2a155da007503c9
mike-yasunov <mike_yasnov@mail.ru>
2023-01-18 15:59:21 +0300

    Update pipeline.py

--- app/pipeline/pipeline.py
+++ app/pipeline/pipeline.py
@@ -30,22 +30,35 @@ LABEL_ENCODER_PATH = 'app/pipeline/src/le.sav'
 
 
 class Pipeline():
-    def __init__(self, data, claffifier_type='CNN', regression_type='Cat'):
-        # self.class_model = ClfModelTabCNN(input_dim=15600,output_dim=3)
+    def __init__(self, data, claffifier_type='CNN', regressor_type='Cat'):
+        '''
+        Args:
+        data: data for prediction 
+        classifier_type: Classification model name (CNN, Cat (Catboost), Lama (LightAutoMl by Sber), Fedot (AutoMl by ITMO))
+        regressor_type: Regression mode name (Cat (Catboost), Lama (LightAutoMl by Sber), Fedot (AutoMl by ITMO))
+        '''
+
+        self.class_model_name = claffifier_type
+        self.reg_model_name = regressor_type
+
         self.class_model = CLASSIFICATION_MODELS_DICT[claffifier_type]
-        # self.class_model.load_state_dict(torch.load(CLASSIFICATION_MODEL_PATH, map_location='cpu'))
         self.class_model.eval()
-        self.reg_model = REGRESSION_MODELS_DICT[regression_type]
+
+        self.reg_model = REGRESSION_MODELS_DICT[regressor_type]
         self.label_encoder = pickle.load(open(LABEL_ENCODER_PATH, 'rb'))
 
         self.X = data
 
     def get_classification(self, ):
         X = self.X[-5200:]
-        X = torch.tensor(X, dtype=torch.float).reshape(1, 5200)
-        logits = self.class_model(X)
-        prediction = logits.argmax(1).numpy()
-
+        if self.class_model_name == 'CNN':
+            X = torch.tensor(X, dtype=torch.float).reshape(1, 5200)
+            logits = self.class_model(X)
+            prediction = logits.argmax(1).numpy()
+        elif self.class_model_name in ['Cat', 'Lama', 'Fedot']:
+            prediction = self.class_model.predict()
+        else:
+            raise Exception('No pretrained model for this type. Please check the name')
         decoded_prediction = self.label_encoder.inverse_transform(prediction)[0]
         return decoded_prediction
 
